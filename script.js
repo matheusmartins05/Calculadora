81133c076b87e3be545c78582ab0ea32e8d8d24c
Matheus Martins <matheusmartins2901@gmail.com>
2022-09-27 22:50:40 -0300

    Layout finalizado

---
+++ script.js
@@ -0,0 +1,12 @@
+//Selecionando elementos//
+
+let resultado = document.querySelector('.resultadoArea')
+let zero = document.querySelector('.zero');
+let um = document.querySelector('.um');
+
+
+
+zero.onclick = () => {
+    let zero1 = 0
+    resultado.textContent = zero1;
+}
